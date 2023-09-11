4403ed4925dae23dadf6be6f7e98b22c179ffd22
Ignatkovich Valery <viignat3@mts.ru>
2023-09-11 16:40:39 +0300

    change orderPreference

--- server.ts
+++ server.ts
@@ -1,11 +1,11 @@
 import 'zone.js/node';
 
-import { APP_BASE_HREF } from '@angular/common';
-import { ngExpressEngine } from '@nguniversal/express-engine';
+import {APP_BASE_HREF} from '@angular/common';
+import {ngExpressEngine} from '@nguniversal/express-engine';
 import * as express from 'express';
-import { existsSync } from 'node:fs';
-import { join } from 'node:path';
-import { AppServerModule } from './src/main.server';
+import {existsSync} from 'node:fs';
+import {join} from 'node:path';
+import {AppServerModule} from './src/main.server';
 
 const expressStaticGzip = require('express-static-gzip');
 
@@ -28,7 +28,7 @@ export function app(): express.Express {
   // Serve static files from /browser
   server.use('/', expressStaticGzip(distFolder, {
     enableBrotli: true,
-    orderPreference: ['br']
+    orderPreference: ['br', 'gz']
   }));
 
   // All regular routes use the Universal engine
